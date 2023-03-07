659e1abab93d4e39ad4afbf3b3798a5566fb89e8
천세준 <cheonsejun@cheonsejun-ui-MacBookAir.local>
2023-03-07 13:00:27 +0900

    Day-1 study

--- Day-1/main.py
+++ Day-1/main.py
@@ -1,1 +1,7 @@
 print('hello world')
+
+#파이썬은 문자열을 더하기로 더할 수 있다.
+print('hello' + " " + 'cheon')
+
+#print 안에 input 함수를 입력하면 입력이 먼저 실행되고 출력을 할 수 있다!
+print("hello " + input("what is your name: ")) 
---
+++ Day-1/project.py
@@ -0,0 +1,5 @@
+print("hello user!")
+hometown = input("where were you grew up? :\n")
+name_of_pet = input("what's your pet's name? :\n")
+
+print("your band name recommended is : " + hometown + name_of_pet)
